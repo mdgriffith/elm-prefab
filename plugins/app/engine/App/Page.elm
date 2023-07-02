0e9341c8f1ed8b44f19efd150e7d30f60e8e506d
Matthew Griffith <mdg.griffith@gmail.com>
2023-07-02 08:02:48 -0400

    Add Page.Init to Page

--- plugins/app/engine/App/Page.elm
+++ plugins/app/engine/App/Page.elm
@@ -1,6 +1,8 @@
 module App.Page exposing
     ( Page, page
+    , Init, init, initWith, notFound, loadFrom
     , withUrlSync
+    , InitPlan(..), toInternalDetails, mapInitPlan
     , toInit, toUpdate, toSubscriptions, toView, toUrlSync
     )
 
@@ -8,10 +10,14 @@ module App.Page exposing
 
 @docs Page, page
 
+@docs Init, init, initWith, notFound, loadFrom
+
 @docs withUrlSync
 
 
 # Internal Details
+
+@docs InitPlan, toInternalDetails, mapInitPlan
 
 @docs toInit, toUpdate, toSubscriptions, toView, toUrlSync
 
@@ -26,20 +32,20 @@ import App.View
 {-| -}
 type Page params msg model
     = Page
-        { init : params -> App.Shared.Shared -> Maybe model -> ( model, App.Effect.Effect msg )
+        { init : params -> App.Shared.Shared -> Maybe model -> Init msg model
         , update : App.Shared.Shared -> msg -> model -> ( model, App.Effect.Effect msg )
         , subscriptions : App.Shared.Shared -> model -> App.Sub.Sub msg
         , view : App.Shared.Shared -> model -> App.View.View msg
         , urlSync :
             Maybe
-                { toParams : model -> params
+                { toParams : model -> { params : params, addToBrowserHistory : Bool }
                 }
         }
 
 
 {-| -}
 page :
-    { init : params -> App.Shared.Shared -> Maybe model -> ( model, App.Effect.Effect msg )
+    { init : params -> App.Shared.Shared -> Maybe model -> Init msg model
     , update : App.Shared.Shared -> msg -> model -> ( model, App.Effect.Effect msg )
     , subscriptions : App.Shared.Shared -> model -> App.Sub.Sub msg
     , view : App.Shared.Shared -> model -> App.View.View msg
@@ -55,18 +61,87 @@ page options =
         }
 
 
+{-| -}
+type alias Init msg model =
+    InitPlan msg model
+
+
+{-| -}
+type InitPlan msg model
+    = NotFound
+    | Loaded model (App.Effect.Effect msg)
+    | LoadFrom (App.Effect.Effect (InitPlan msg model))
+
+
+mapInitPlan : { onModel : model -> model2, onMsg : msg -> msg2 } -> InitPlan msg model -> InitPlan msg2 model2
+mapInitPlan ({ onModel, onMsg } as fns) initPlan =
+    case initPlan of
+        NotFound ->
+            NotFound
+
+        Loaded model effect ->
+            Loaded (onModel model) (App.Effect.map onMsg effect)
+
+        LoadFrom effect ->
+            LoadFrom (App.Effect.map (mapInitPlan fns) effect)
+
+
+{-| -}
+init : model -> Init msg model
+init model =
+    Loaded model App.Effect.none
+
+
+{-| -}
+initWith : model -> App.Effect.Effect msg -> Init msg model
+initWith model effect =
+    Loaded model effect
+
+
+{-| -}
+notFound : Init msg model
+notFound =
+    NotFound
+
+
+{-| -}
+loadFrom : App.Effect.Effect (Init msg model) -> Init msg model
+loadFrom effect =
+    LoadFrom effect
+
+
 
 {- URL SYNCING -}
 
 
 {-| -}
 withUrlSync :
-    { toParams : model -> params
-    }
+    { toParams : model -> { params : params, addToBrowserHistory : Bool } }
     -> Page params msg model
     -> Page params msg model
 withUrlSync urlSync (Page details) =
     Page { details | urlSync = Just urlSync }
+
+
+
+{- Internal -}
+
+
+{-| -}
+toInternalDetails :
+    Page params msg model
+    ->
+        { init : params -> App.Shared.Shared -> Maybe model -> ( model, App.Effect.Effect msg )
+        , update : App.Shared.Shared -> msg -> model -> ( model, App.Effect.Effect msg )
+        , subscriptions : App.Shared.Shared -> model -> App.Sub.Sub msg
+        , view : App.Shared.Shared -> model -> App.View.View msg
+        , urlSync :
+            Maybe
+                { toParams : model -> { params : params, addToBrowserHistory : Bool }
+                }
+        }
+toInternalDetails (Page details) =
+    details
 
 
 {-| -}
