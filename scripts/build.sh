bcafd9d38e09dee4df52397fdce2ac0a90c5fec5
Matthew Griffith <mdg.griffith@gmail.com>
2023-10-14 09:12:57 -0400

    Include interactive in the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,9 @@ node scripts/build.js
     cd plugins/theme
     elm-optimize-level-2 Generate.elm --output=../../cli/generators/theme.js
 )
-# (
-#     cd plugins/interactive
-#     elm-optimize-level-2 Generate.elm --output=../../cli/generators/interactive.js
-# )
+(
+    cd plugins/interactive
+    elm-optimize-level-2 Generate.elm --output=../../cli/generators/interactive.js
+)
 # tsc
 esbuild cli/run.ts --bundle --outfile=dist/elm-press.js --platform=node
