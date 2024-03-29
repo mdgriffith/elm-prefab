06a545ff3e8bd25a9fa2cc3c0feda3406d946833
Matthew Griffith <mdg.griffith@gmail.com>
2024-03-29 12:42:07 -0400

    Bump to 0.1.1 - Better summary reporting, maybe resolved dep issues?

--- cli/templates/app/toJs.ts
+++ cli/templates/app/toJs.ts
@@ -5,16 +5,16 @@ import * as Options from "../../options";
 
 export const copyTo = (baseDir: string, overwrite: boolean, summary: Options.Summary) => { 
   
-  if (overwrite || !fs.existsSync(path.join(baseDir, "/webcomponents/clipboard.ts"))) {
-    const filepath = path.join(baseDir, "/webcomponents/clipboard.ts");
+  if (overwrite || !fs.existsSync(path.join(baseDir, "/js/clipboard.ts"))) {
+    const filepath = path.join(baseDir, "/js/clipboard.ts");
     fs.mkdirSync(path.dirname(filepath), { recursive: true });
     fs.writeFileSync(filepath, "export function copy(text: string) {\n  const clipboard = navigator.clipboard;\n  if (!clipboard) {\n    return;\n  }\n  clipboard.writeText(text);\n}\n");
     const generated = { outputDir: baseDir, path: filepath}
     Options.addGenerated(summary, generated);
   }
 
-  if (overwrite || !fs.existsSync(path.join(baseDir, "/webcomponents/localStorage.ts"))) {
-    const filepath = path.join(baseDir, "/webcomponents/localStorage.ts");
+  if (overwrite || !fs.existsSync(path.join(baseDir, "/js/localStorage.ts"))) {
+    const filepath = path.join(baseDir, "/js/localStorage.ts");
     fs.mkdirSync(path.dirname(filepath), { recursive: true });
     fs.writeFileSync(filepath, "export const getAll = () => {\n  const data: any = {};\n  for (var i = 0, len = localStorage.length; i < len; ++i) {\n    const key = localStorage.key(i);\n    if (key) {\n      data[key] = get(key);\n    }\n  }\n  return data;\n};\n\nexport const get = (key: string): any => {\n  const item = localStorage.getItem(key);\n  if (item) {\n    try {\n      return JSON.parse(item);\n    } catch (e) {\n      return null;\n    }\n  } else {\n    return null;\n  }\n};\n\nexport const set = (key: string, value: any) => {\n  localStorage.setItem(key, JSON.stringify(value));\n};\n\nexport const clear = (key: string) => {\n  localStorage.removeItem(key);\n};\n");
     const generated = { outputDir: baseDir, path: filepath}
@@ -32,7 +32,7 @@ export const copyTo = (baseDir: string, overwrite: boolean, summary: Options.Sum
   if (overwrite || !fs.existsSync(path.join(baseDir, "/main.ts"))) {
     const filepath = path.join(baseDir, "/main.ts");
     fs.mkdirSync(path.dirname(filepath), { recursive: true });
-    fs.writeFileSync(filepath, "// @ts-ignore\nimport { Elm } from \"../src/Main.elm\";\nimport * as Clipboard from \"./webcomponents/clipboard\";\nimport * as LocalStorage from \"./webcomponents/localStorage\";\n\n// Boot up the Elm App\nconst app = Elm.Main.init({\n  flags: { now: Date.now(), localStorage: LocalStorage.getAll() },\n});\n\n// Handling data from elm to JS\napp.ports?.outgoing?.subscribe?.((message: any) => {\n  switch (message.tag) {\n    case \"local-storage\":\n      LocalStorage.set(message.details.key, message.details.value);\n      if (app.ports?.localStorageUpdated) {\n        app.ports.localStorageUpdated.send(message.details);\n      }\n\n      break;\n\n    case \"local-storage-clear\":\n      LocalStorage.clear(message.details.key);\n      break;\n\n    case \"copy-to-clipboard\":\n      Clipboard.copy(message.details.text);\n      break;\n\n    default:\n      break;\n  }\n});\n");
+    fs.writeFileSync(filepath, "// @ts-ignore\nimport { Elm } from \"../src/Main.elm\";\nimport * as Clipboard from \"./js/clipboard\";\nimport * as LocalStorage from \"./js/localStorage\";\n\n// Boot up the Elm App\nconst app = Elm.Main.init({\n  flags: { now: Date.now(), localStorage: LocalStorage.getAll() },\n});\n\n// Handling data from elm to JS\napp.ports?.outgoing?.subscribe?.((message: any) => {\n  switch (message.tag) {\n    case \"local-storage\":\n      LocalStorage.set(message.details.key, message.details.value);\n      if (app.ports?.localStorageUpdated) {\n        app.ports.localStorageUpdated.send(message.details);\n      }\n\n      break;\n\n    case \"local-storage-clear\":\n      LocalStorage.clear(message.details.key);\n      break;\n\n    case \"copy-to-clipboard\":\n      Clipboard.copy(message.details.text);\n      break;\n\n    default:\n      break;\n  }\n});\n");
     const generated = { outputDir: baseDir, path: filepath}
     Options.addGenerated(summary, generated);
   }
