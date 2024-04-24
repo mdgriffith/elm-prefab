55c8c5907a0679879eb8d072cb6261daf41e7722
Matthew Griffith <mdg.griffith@gmail.com>
2024-04-24 10:04:28 -0400

    Stop generating the meta.json file from esbuild

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,4 @@ node scripts/build.js
 
 # tsc
 # esbuild cli/run.ts --bundle --outfile=dist/elm-prefab.js --platform=node --bundle --minify --pure:A2 --pure:A3 --pure:A4 --pure:A5 --pure:A6 --pure:A7 --pure:A8 --pure:A9 --pure:F2 --pure:F3 --pure:F3 --pure:F4 --pure:F5 --pure:F6 --pure:F7 --pure:F8 --pure:F9 
-esbuild cli/run.ts --bundle --outfile=dist/elm-prefab.js --platform=node --bundle --metafile=meta.json
+esbuild cli/run.ts --bundle --outfile=dist/elm-prefab.js --platform=node
