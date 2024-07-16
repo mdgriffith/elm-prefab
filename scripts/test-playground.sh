d4058324bf9eca636ed3c070be0786fa0a1a7a25
Matthew Griffith <mdg.griffith@gmail.com>
2024-07-15 20:40:52 -0400

    Update testing script

--- scripts/test-playground.sh
+++ scripts/test-playground.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
-cd examples/internal-testing
+cd examples/test
 
 rm -rf docs/.elm-prefab
 
 node ../../dist/elm-prefab.js
 
-cd docs
-elm make src/Main.elm --output=/dev/null
-
 pnpm install
 pnpm build
 pnpm dev
+
+# cd docs
+# elm make src/Main.elm --output=/dev/null
+
+# pnpm install
+# pnpm build
+# pnpm dev
