f6a3a5bc1431f2c28b603959f63eab0b6f35edd2
Matthew Griffith <mdg.griffith@gmail.com>
2024-07-31 08:36:25 -0400

    compile stuff

--- cli/templates/theme/copyAll.ts
+++ cli/templates/theme/copyAll.ts
@@ -1,12 +1,10 @@
 
 import * as Options from "../../options";
-import * as toHidden from "./toHidden";
 import * as toSrc from "./toSrc";
 import * as toJs from "./toJs";
 
 
 export const copy = (options: Options.RunOptions, summary: Options.Summary) => {
-  toHidden.copyTo(options.internalSrc, true, false, summary)
   toSrc.copyTo(options.src, false, !options.generateDefaultFiles, summary)
   toJs.copyTo(options.js, false, !options.generateDefaultFiles, summary)
 }
