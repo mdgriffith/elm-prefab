5c18974dbdfc0069f26de2439026ceb1300cccf4
Matthew Griffith <mdg.griffith@gmail.com>
2024-09-12 11:19:32 -0400

    Chain init test commands together

--- scripts/init-test.sh
+++ scripts/init-test.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-
-cd examples
-rm -rf test
-mkdir test
-cd test
-
-node ../../dist/elm-prefab.js
-
-elm make src/app/Main.elm --output=/dev/null
-
-pnpm install
+cd examples && \
+rm -rf test && \
+mkdir test && \
+cd test && \
+node ../../dist/elm-prefab.js init && \
+elm make src/app/Main.elm --output=/dev/null && \
+pnpm install && \
 pnpm build
 # pnpm dev
