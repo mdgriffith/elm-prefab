c0eea0d0d861deef08394e66f6dccbb07545a891
Matthew Griffith <mdg.griffith@gmail.com>
2024-09-15 13:46:35 -0400

    Use env file for graphQL stuff

--- scripts/init-test.sh
+++ scripts/init-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -a
+source .env
+set +a
 
 cd examples && \
 rm -rf test && \
@@ -6,6 +9,7 @@ mkdir test && \
 cd test && \
 node ../../dist/elm-prefab.js init && \
 elm make src/app/Main.elm --output=/dev/null && \
+node ../../dist/elm-prefab.js add graphql && \
+node ../../dist/elm-prefab.js generate && \
 pnpm install && \
 pnpm build
-# pnpm dev
