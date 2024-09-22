ea2ad6790cda585c1b50f521af0e2e59a34906ce
Matthew Griffith <mdg.griffith@gmail.com>
2024-09-22 09:33:44 -0400

    Add Copy.copyPlugin

--- cli/copy.ts
+++ cli/copy.ts
@@ -42,3 +42,45 @@ export const copy = (runOptions: Options.RunOptions): Options.Summary => {
 
   return summary;
 };
+
+export const copyPlugin = (
+  pluginToRun: string,
+  runOptions: Options.RunOptions,
+): Options.Summary => {
+  const summary: Options.Summary = { generated: [] };
+
+  // Manage hidden files
+  for (const plugin of Customizable.all) {
+    if (plugin.plugin == pluginToRun) {
+      for (const file of plugin.all) {
+        if (runOptions.project.userControlled.has(file.path)) {
+          // User controlled
+          if (fs.existsSync(path.join(runOptions.internalSrc, file.path))) {
+            // If exists in hidden dir, remove it
+            fs.unlinkSync(path.join(runOptions.internalSrc, file.path));
+          }
+        } else {
+          // Prefab controlled, copy to hidden
+          const fullFilePath = path.join(runOptions.internalSrc, file.path);
+          const generated = {
+            outputDir: runOptions.internalSrc,
+            path: fullFilePath,
+          };
+          fs.mkdirSync(path.dirname(fullFilePath), { recursive: true });
+          fs.writeFileSync(fullFilePath, file.contents);
+          Options.addGenerated(summary, generated);
+        }
+      }
+    }
+  }
+
+  if (runOptions.generateDefaultFiles) {
+    for (const plugin of CopyAll.all) {
+      if (plugin.plugin == pluginToRun) {
+        plugin.copy(runOptions, summary);
+      }
+    }
+  }
+
+  return summary;
+};
