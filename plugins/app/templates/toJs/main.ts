6e3fd874ee6f7ec722d5da034d64e46cc46a6234
Matthew Griffith <mdg.griffith@gmail.com>
2024-11-03 08:42:07 -0500

    Import all generated stylesheets in the default main.ts

--- plugins/app/templates/toJs/main.ts
+++ plugins/app/templates/toJs/main.ts
@@ -4,6 +4,9 @@ import * as LocalStorage from "./js/local-storage";
 import * as Ports from "./js/ports";
 import Webcomponents from "./js/webcomponents";
 
+// Import all generated CSS files
+import.meta.glob("../.elm-prefab/**/*.css", { eager: true });
+
 // Include any custom elements we need.
 Webcomponents();
 
