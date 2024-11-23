69188e063c6edcede40fae11fa727d581d3db06b
Matthew Griffith <mdg.griffith@gmail.com>
2024-11-23 10:52:42 -0500

    Test docs site

--- scripts/init-test.sh
+++ scripts/init-test.sh
@@ -3,13 +3,17 @@ set -a
 source .env
 set +a
 
-cd examples && \
+cd playground && \
 rm -rf test && \
 mkdir test && \
 cd test && \
 node ../../dist/elm-prefab.js init && \
 elm make src/app/Main.elm --output=/dev/null && \
-node ../../dist/elm-prefab.js add graphql && \
-node ../../dist/elm-prefab.js generate && \
-pnpm install && \
+# node ../../dist/elm-prefab.js add graphql && \
+# node ../../dist/elm-prefab.js generate && \
+# pnpm install && \
+# pnpm build && \
+# node ../../dist/elm-prefab.js add && \
+node ../../dist/elm-prefab.js add docs && \
+node ../../dist/elm-prefab.js add theme && \
 pnpm build
